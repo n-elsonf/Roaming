7de868171104d08c44a2183e5c9d53521c78cb63
Nelson <nelsonfeng.18@gmail.com>
2025-07-09 00:51:44 -0400

    set up mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
-// import express from "express";
-const express = require("express");
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -39,3 +38,16 @@ const upload = multer({ storage });
 app.listen(5001, () => {
   console.log("Server started on PORT: 5001");
 });
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  // .connect(process.env.MONGO_URL, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // })
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
