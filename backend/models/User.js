ada49f7d1c5d8365fc7e40c09655c5b73d67b4d5
Nelson <nelsonfeng.18@gmail.com>
2025-07-09 17:14:11 -0400

    make auth controller and user model

---
+++ backend/models/User.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 7,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: {
+      type: String,
+    },
+    itineraries: {
+      type: Array,
+      default: [],
+    },
+    itinerariesCreated: {
+      type: Number,
+    },
+  },
+  { timestamps: true },
+);
+
+// When creating a model, create a schema and then use mongoose.model to represent data object
+const User = mongoose.model("User", UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
 app.listen(5001, () => {
   console.log("Server started on PORT: 5001");
