8acf385e6b5a5cc0165efc017846c9553cf3bd17
Nelson <nelsonfeng.18@gmail.com>
2025-07-09 00:34:16 -0400

    more npm installs and prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Roaming
+
 An app aimed towards connecting strangers by sharing travel itineraries together. :)
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,41 @@
 // import express from "express";
 const express = require("express");
-const app = express()
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.listen(5001, () => {
   console.log("Server started on PORT: 5001");
-})
+});
